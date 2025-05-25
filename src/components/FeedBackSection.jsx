00e79c583c5b203bd9b12154fbf8fe3eaec05e37
VValentinesDay <walter9retlaw@gmail.com>
2025-05-25 16:33:33 +0300

    пример блока с useState

--- src/components/FeedBackSection.jsx
+++ src/components/FeedBackSection.jsx
@@ -4,27 +4,68 @@ import "../index.css"
 import Button from "./Button/Button"
 
 
-export default function FeedBackSection(){
-    // отдельное состояние для наличия ошибки 
-    const [hasError, setHasError] = useState(false) 
 
-    const [name, setName] = useState('') // по умолчанию, имя(начальное состояние) - пустая строчка
-    // передача state в форму ввода input передаётся с помощью слова value
-    const [reason, setReason] = useState('Reason_1s')
+function StateVsRef() {
 
-    // функция для взаимодействия с input 
-    function handleNameChange(event) {
-        // console.log(event.target.value)
-        // дайт доступ к данным собыития
-        setName(event.target.value)
-        // state не меняется пока не выполнится оставшаяся часть функции, т.е. рендеринга не будет
-       // неверный пример: setHasError(name.trim().length == 0 )
-       // правильно
-        setHasError(event.target.value.trim().length == 0 )
+    const[textValue, setTextValue] = useState('nothing inside')
+
+    function handleSpecialInput(event)
+    {
+        setTextValue(event.target.value)
     }
 
 
-    // работа с предыдущим остоянием
+   return ( <div>
+        <h3>
+            Input Value : {textValue}
+        </h3>
+        <input type="text" className="control" onChange={handleSpecialInput}>
+
+        </input>
+    </div>)
+}
+
+export default function FeedBackSection(){
+// использование единого use state 
+// данные объекта типа name, можно вызывать из form 
+    const [form, setForm] = useState({
+        name: '',
+        hasError : true,
+        reason: 'Reason_1'
+    })
+
+    // // отдельное состояние для наличия ошибки 
+    // const [hasError, setHasError] = useState(false) 
+
+    // const [name, setName] = useState('') // по умолчанию, имя(начальное состояние) - пустая строчка
+    // // передача state в форму ввода input передаётся с помощью слова value
+    // const [reason, setReason] = useState('Reason_1s')
+
+    // функция для взаимодействия с input 
+    function handleNameChange(event) {
+    //     // console.log(event.target.value)
+    //     // дайт доступ к данным собыития
+    //     setName(event.target.value)
+    //     // state не меняется пока не выполнится оставшаяся часть функции, т.е. рендеринга не будет
+    //    // неверный пример: setHasError(name.trim().length == 0 )
+    //    // правильно
+    //     setHasError(event.target.value.trim().length == 0 )
+
+    // единый UseState
+    // setForm({name: event.target.value,
+    //         hasError: event.target.value.trim().length == 0,
+    //         reason: form.reason
+    //  })
+    
+    setForm((previos) => ({
+        ...previos,
+        name: event.target.value,
+        hasError: event.target.value.trim().length == 0
+    }))
+}
+
+
+    // // работа с предыдущим остоянием
     function toggleError(){
         // setHasError(!hasError) // после вызова true (по умолчанию) изменитя на false
         // setHasError(!hasError) // однако при повотоном вызове по логике значение должно поменяться ещё раз,
@@ -33,44 +74,45 @@ export default function FeedBackSection(){
         // setHasError((previos) => !previos )
         // setHasError((previos) => !previos )
         // // в таком случае значение не поменятеся
-        setHasError((previos) => !previos )
-
-
-
-    }
+        // setHasError((previos) => !previos )
+     }
 
     return(
         <section>
             <h3>обратная связь</h3> 
 
             <Button onClick={toggleError}>Toggle Error</Button>
-            <form>
+            
+            <form style={{marginBottom: '1rem'}}>
                 <label htmlFor="name">Ваше имя </label>
                  <input 
                  className="inputStyle" 
                  type="text" id="name" 
-                 value={name} 
+                 value={form.name} 
                  onChange={handleNameChange}
                 //первые фигурные кобки говорят что будет динамика, вторые - что будет объект
                 style={{
-                    border: name.trim().length ? null : '1px solid red' // если в  длине что-то есть, тогда мы ничего не добавляем иначе ...
+                    border: form.name.trim().length ? null : '1px solid red' // если в  длине что-то есть, тогда мы ничего не добавляем иначе ...
                     // с использованием состояния
                     // border: hasError ?  '1px solid red' : null 
                }}  />
-
+                
 
                  <label htmlFor="reason">Причина обращения</label>
-                 <select className="control" id="reason" value={reason} onChange={event => setReason(event.target.value)}>
+                 <select className="control" id="reason" value={form.reason} onChange={(event) => setForm(prev => ({...prev, reason: event.target.value}))}>
                     <option value="Reason_1">Причина 1</option>
                     <option value="Reason_2">Причина 2</option>
                     <option value="Reason_3">Причина 3 </option>
                  </select>
                  <pre>
-                    Name: {name}
+                    {/* Name: {form.name}
                     <br/> 
-                    Reason: {reason}
+                    Reason: {form.reason} */}
+                    {JSON.stringify(form, null, 2)}
                  </pre>
-                 <Button disabled ={hasError} isActive={!hasError}>Отправить</Button>
+                 <Button disabled ={form.hasError} isActive={!form.hasError}>Отправить</Button>
+                 <hr style={{marginBottom: '30px'}}/>
+                 <StateVsRef></StateVsRef>
             </form>
         </section>
     )
